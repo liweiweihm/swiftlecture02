7e61ea3533bb7c6d0195b47694b18745d55a6753
liweiweihm <liweiweihnzk@163.com>
2018-07-22 16:12:56 +0800

    name4

--- swiftlecture02/ViewController.swift
+++ swiftlecture02/ViewController.swift
@@ -17,16 +17,21 @@ class ViewController: UIViewController {
         var ints : [Int] = [2,1,3]
         var floots : Array<Float> = Array(repeating: 0,count: 5)
         ints.sort()
-        for index in 0..<3{
-            print("\(english[index])")
-        }
-        for str in english{
-            print(str)
-        }
-        let 吧 = [ 1 : "A" , 2: "C" , 3 : "B" ]
-        for(a,b) int 吧{
+//        for index in 0..<3{
+//            print("\(english[index])")
+//        }
+//        for str in english{
+//            print(str)
+//        }
+        let dic = [ 1 : "A" , 2: "C" , 3 : "B" ]
+        for(a,b) int dic{
             print("\( a ):\(b)")
         }
+        let dicSorted = dic.sorted(by: { $0.value<$1.value})
+        for( a, b) in dicSorted {
+            print("\( a ):( b )")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
